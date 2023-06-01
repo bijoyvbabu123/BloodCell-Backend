a2a51a125158cc3a03ba4ea7d9c0c92c2a65266c
bijoyvbabu123 <73835543+bijoyvbabu123@users.noreply.github.com>
2023-06-01 11:07:41 +0530

    user model and manager setup

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import User
+
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'is_email_verified', 'date_joined', 'is_active', 'is_staff')
+    list_filter = ('is_email_verified', 'is_staff', 'is_active', 'is_superuser')
+    search_fields = ('email', 'date_joined')
+    ordering = ('email',)
+
+
+
+
+
+admin.site.register(User, UserAdmin)
